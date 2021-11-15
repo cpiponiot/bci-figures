fcaceed2803807c8a8739cb63cf5a94e87e61e4b
Camille Piponiot <camille.piponiot@gmail.com>
2021-11-15 16:32:17 +0100

    main script - load data

--- main-script.R
+++ main-script.R
@@ -17,7 +17,12 @@ if (length(ins_packages) > 0)
 # load all packages
 lapply(req_packages, require, character.only = TRUE)
 
-# download BCI data from the Dryad repository
+# source internal functions 
+# source("functions.R")
+
+### Get BCI 50-ha plot data ####
+
+# download BCI data from the Dryad repository 
 # citation: Condit, Richard et al. (2019), Complete data from the Barro Colorado
 # 50-ha plot: 423617 trees, 35 years, Dryad, Dataset,
 # https://doi.org/10.15146/5xcp-0d46
@@ -29,5 +34,30 @@ zip_folders <- sapply(zip_files, function(dir) {
   name <- gsub("\\.zip", "", data.table::last(strsplit(dir, '/')[[1]]))
   utils::unzip(dir, exdir = name)
 })
+
+# load species table
+load(grep("spp", dryad_data_path$`10.15146/5xcp-0d46`, value = TRUE))
+
+# load stem censuses
+# 1. list census files
+bci_stem <- list.files("bci.stem")
+
+# 2. load census files as list
+census_list <- lapply(bci_stem, function(name) {
+  load(paste0("bci.stem/", name)); get(strsplit(name, "\\.rda")[[1]][1])
+})
+
+# 3. add census number information as list names
+names(census_list) <- data.table::tstrsplit(bci_stem, "\\.")[[2]]
+
+# 4. compile as one data table
+df_census <- data.table::rbindlist(census_list, fill = TRUE, idcol = "censusID")
+
+
+
+
+
+
+
 
 
